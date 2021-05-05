8c430206e7145c0dcbaf2df484b92607a1a0bccb
Waide Soper <waide.soper@gmail.com>
2021-05-05 10:41:10 +1000

    Added in 3 internal commands - received_sub_gift, received_bit_donation, received_donation

--- src/main/java/iskallia/vault/command/InternalCommand.java
+++ src/main/java/iskallia/vault/command/InternalCommand.java
@@ -1,7 +1,21 @@
 package iskallia.vault.command;
 
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import iskallia.vault.util.EntityHelper;
+import iskallia.vault.item.ItemGiftBomb;
+import iskallia.vault.item.ItemGiftBomb.Variant;
+import iskallia.vault.item.ItemTraderCore;
+import iskallia.vault.item.ItemTraderCore.CoreType;
+import iskallia.vault.command.GiveBitsCommand;
 import net.minecraft.command.CommandSource;
+import net.minecraft.command.Commands;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.ItemStack;
+
 
 public class InternalCommand extends Command {
 
@@ -16,7 +30,58 @@ public class InternalCommand extends Command {
     }
 
     @Override
-    public void build(LiteralArgumentBuilder<CommandSource> builder) { }
+    public void build(LiteralArgumentBuilder<CommandSource> builder) { 
+        builder.then(
+            Commands.literal("received_sub_gift")
+                    .then(Commands.argument("actor", StringArgumentType.word())
+                    .then(Commands.argument("amount", IntegerArgumentType.integer())
+                    .then(Commands.argument("tier", IntegerArgumentType.integer(0,3))
+                    .executes(context -> receivedSubGift(context)))))
+        );
+        builder.then(
+            Commands.literal("received_donation")
+                    .then(Commands.argument("actor", StringArgumentType.word())
+                    .then(Commands.argument("amount", IntegerArgumentType.integer())
+                    .executes(context -> receivedDonation(context))))
+        );
+        builder.then(
+            Commands.literal("received_bit_donation")
+                    .then(Commands.argument("actor", StringArgumentType.word())
+                    .then(Commands.argument("amount", IntegerArgumentType.integer())
+                    .executes(context -> receivedBitDonation(context))))
+        );
+    }
+
+    private static int receivedSubGift(CommandContext<CommandSource> context) throws CommandSyntaxException {
+        String actor = StringArgumentType.getString(context, "actor");
+        int amount = IntegerArgumentType.getInteger(context,"amount");
+        int tier = IntegerArgumentType.getInteger(context,"tier");
+        CommandSource source = context.getSource();
+        if(tier > 3 || tier < 0){
+            throw new InternalError("Unknown tier -> " + tier);
+        } 
+        ItemStack item = ItemGiftBomb.forGift(Variant.values()[tier], actor, amount);
+        EntityHelper.giveItem(context.getSource().asPlayer(), item);
+        return 0;
+    }
+    
+    private static int receivedDonation(CommandContext<CommandSource> context) throws CommandSyntaxException {
+        String actor = StringArgumentType.getString(context, "actor");
+        int amount = IntegerArgumentType.getInteger(context,"amount");
+        CommandSource source = context.getSource();
+        boolean isMegaHead = amount >= 10;
+        ItemStack item = ItemTraderCore.generate(actor,amount,isMegaHead,CoreType.COMMON);
+        EntityHelper.giveItem(context.getSource().asPlayer(), item);
+        return 0;
+    }
+
+    private static int receivedBitDonation(CommandContext<CommandSource> context) throws CommandSyntaxException {
+        String actor = StringArgumentType.getString(context, "actor");
+        int amount = IntegerArgumentType.getInteger(context,"amount");
+        CommandSource source = context.getSource();
+        GiveBitsCommand.dropBits(context.getSource().asPlayer(), amount);
+        return 0;
+    }
 
     @Override
     public boolean isDedicatedServerOnly() {
