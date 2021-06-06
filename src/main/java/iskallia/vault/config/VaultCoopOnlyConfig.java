72cc50ef750525817638f1eed53f13e5d4d43c79
Ontikarus Ryan <realddrkhat@gmail.com>
2021-06-06 11:16:09 +0100

    Option to equalise exp from a co-op vault

--- src/main/java/iskallia/vault/config/VaultCoopOnlyConfig.java
+++ src/main/java/iskallia/vault/config/VaultCoopOnlyConfig.java
@@ -6,12 +6,19 @@ public class VaultCoopOnlyConfig extends Config{
     
     @Expose
     public Boolean IS_COOP_ONLY;
+    public Boolean VAULT_EXP_EQUAL;
     
     public String getName() {
         return "vault_coop_only";
     }
+    public String getXPShare() {
+        return "vault_exp_equal";
+    }
 
     public void reset(){
         this.IS_COOP_ONLY=false;
+    }
+    public void resetXPShare(){
+        this.VAULT_EXP_EQUAL=false;
     }
 }
